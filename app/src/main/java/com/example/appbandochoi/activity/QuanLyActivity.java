2f79e1614501009c23db26ee424de3320dc75ae8
Nguyenvsang <80632518+Nguyenvsang@users.noreply.github.com>
2023-04-13 21:37:56 +0700

    cập nhật giao diện quản lý

---
+++ app/src/main/java/com/example/appbandochoi/activity/QuanLyActivity.java
@@ -0,0 +1,16 @@
+package com.example.appbandochoi.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.appbandochoi.R;
+
+public class QuanLyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quan_ly);
+    }
+}
