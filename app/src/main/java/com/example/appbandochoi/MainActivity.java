8af840d39847f640f47476b7cc42e22141df472a
Nguyenvsang <80632518+Nguyenvsang@users.noreply.github.com>
2023-04-16 14:58:25 +0700

    cập nhật giao diện home 3.0

--- app/src/main/java/com/example/appbandochoi/MainActivity.java
+++ app/src/main/java/com/example/appbandochoi/MainActivity.java
@@ -23,59 +23,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    ViewFlipper viewFlipper;
-    RecyclerView recyclerViewManHinhChinh;
-    NavigationView navigationView;
-    ListView listViewManHinhChinh;
-    DrawerLayout drawerLayout;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Anhxa();
-        ActionBar();
-        ActionViewFlipper();
-    }
-
-    private void ActionViewFlipper() {
-        List<String> mangquangcao = new ArrayList<>();
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-Le-hoi-phu-kien-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-HC-Tra-Gop-800-300.png");
-        mangquangcao.add("http://mauweb.monamedia.net/thegioididong/wp-content/uploads/2017/12/banner-big-ky-nguyen-800-300.jpg");
-        for (int i = 0; i<mangquangcao.size(); i++){
-            ImageView imageView = new ImageView(getApplicationContext());
-            Glide.with(getApplicationContext()).load(mangquangcao.get(i)).into(imageView);
-            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
-            viewFlipper.addView(imageView);
-        }
-        viewFlipper.setFlipInterval(3000);
-        viewFlipper.setAutoStart(true);
-        Animation slide_in = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_in_right);
-        Animation slide_out = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide_out_right);
-        viewFlipper.setInAnimation(slide_in);
-        viewFlipper.setOutAnimation(slide_out);
-    }
-
-    private void ActionBar() {
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        toolbar.setNavigationIcon(android.R.drawable.ic_menu_sort_by_size);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                drawerLayout.openDrawer(GravityCompat.START);
-            }
-        });
-    }
-
-    private void Anhxa() {
-        toolbar = findViewById(R.id.toobarmanhinhchinh);
-        viewFlipper = findViewById(R.id.viewlipper);
-        recyclerViewManHinhChinh = findViewById(R.id.recycleview);
-        listViewManHinhChinh = findViewById(R.id.listviewmanhinhchinh);
-        navigationView = findViewById(R.id.navigationview);
-        drawerLayout = findViewById(R.id.drawerlayout);
     }
 }
