61d91f5a9a73839daac70290159f08f00fd4a75a
qifag <118126747+qifag@users.noreply.github.com>
2023-05-11 09:29:33 +0700

    Giao diện cho người quản lý

--- app/src/main/java/com/example/appbandochoi/constants/Constants.java
+++ app/src/main/java/com/example/appbandochoi/constants/Constants.java
@@ -1,6 +1,6 @@
 package com.example.appbandochoi.constants;
 
 public class Constants {
-    public static final String BASE_URL = "http://192.168.239.2:8080/toystoreapp/";
-    public static final String ROOT_URL = "http://192.168.239.2:8080";
+    public static final String BASE_URL = "http://192.168.1.4:8080/toystoreapp/";
+    public static final String ROOT_URL = "http://192.168.1.4:8080";
 }
